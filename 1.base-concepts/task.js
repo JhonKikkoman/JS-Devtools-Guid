2b9546907a2658e1921e8cbb9193b98108e87240
Piskun_Ivan <tw1st3r111@mail.ru>
2022-11-01 22:53:47 +0300

    Task2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,20 @@
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  "use strict";
+  const d = (b ** 2 - 4 * a * c);
+  let root1;
+  let root2;
+
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+   let root1 = (-b/(2*a));
+   let calcD = (d - root1);
+   arr.push(calcD);
+  // } else if (d > 0) {
+
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
