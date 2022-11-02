28146cb7ec74b1c8b0475ee1ae114fa484928783
Piskun_Ivan <tw1st3r111@mail.ru>
2022-11-02 22:15:19 +0300

    Fixed2HW

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,18 +1,19 @@
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
   "use strict";
-  const d = (b ** 2 - 4 * a * c);
-  let root1;
-  let root2;
+  let d = (b ** 2 - 4 * a * c);
+  let d2;
 
   if (d < 0) {
     arr = [];
   } else if (d === 0) {
-   let root1 = (-b/(2*a));
-   let calcD = (d - root1);
-   arr.push(calcD);
-  // } else if (d > 0) {
-
+   d = (-b/(2*a));
+   arr.push(d);
+  } else if (d > 0) {
+    d = ((-b + Math.sqrt(d) )/(2*a));
+    d2 = ((-b - Math.sqrt(d) )/(2*a));
+    arr.push(d, d2);
+    console.log(arr);
   }
   return arr;
 }
