eace1384fab2cbd9659a75707753ce7193a5c84d
Piskun_Ivan <tw1st3r111@mail.ru>
2022-11-08 16:04:25 +0300

    HW3arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,32 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = compareArrays.every((array) => sum === sum2);
 
-  // Ваш код
-
+  function sumArray1() {
+    for (let element of arr1) {
+      let sum;
+      sum += arr1[element];
+      break;
+    }
+    return sum;
+  }
+  
+  function sumArray2() {
+    for (let element of arr2) {
+      let sum2;
+      sum2 += arr2[element];
+      break;
+    }
+    return sum2;
+  }
+  
   return result; // boolean
 }
+compareArrays([8, 9], [6]);
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(number => number > 0 ).filter(number => number % 3 == 0).map(number * 10)
 
   return resultArr; // array
 }
