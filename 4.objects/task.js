cf5483194888e8dbcfeca6aa100799735b68a758
Piskun_Ivan <tw1st3r111@mail.ru>
2022-12-02 15:31:15 +0300

    task4.obj

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,37 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.setSubject();
+  this.addMark();
 }
 
 Student.prototype.setSubject = function (subjectName) {
+  return subject = subjectName;
+}
+
+Student.prototype.addMark = function (mark) {
+  const marks = [];
+  if(this.marks === undefined){ 
+    marks.push(mark);
+    } else {
+      marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...args) {
   //ваш код
 }
 
-// ваш код для остальных методов
+Student.prototype.getAverage = function () {
+  // const averSum =
+}
+
+Student.prototype.exclude = function (reason) {
+  delete Student.subject;
+  delete Student.marks;
+  return this.exclude = excluded(reason);
+}
+
+let student1 = new Student("Tony", "male", 37);
+student1.setSubject("Algebra");
+student1.addMark(5);
+student1.addMark(4);
+student1.addMark(5);
