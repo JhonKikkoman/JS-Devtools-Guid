a81e2ee644bebe33d19278a6b59fe837bd520c1b
Piskun_Ivan <tw1st3r111@mail.ru>
2022-12-11 23:02:18 +0300

    task2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,55 +1,42 @@
-// Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  sum = 0;
   min = arr[0];
   max = arr[0];
-
   for (let i = 0; i < arr.length; i += 1){
     if (arr[i] > max) {
       max = arr[i];
     } else if (arr[i] < min) {
       min = arr[i];
     }
+    sum += arr[i];
   }
-  
-  sum += arr[i];
   avg = Number((sum / arr.length).toFixed(2));
-
   return { min: min, max: max, avg: avg };
 }
 
-
-// Задание 2
 function worker(arr) {
-  let sum;
-
+  let sum = 0;
     for (let i = 0; i < arr.length; i += 1){
-    let calc;
-    calc += arr[i];
+    sum += arr[i];
   }
-  
   return (sum = calc);
 }
 
 function makeWork(arrOfArr, func) {
   let max = func(arrOfArr[0]);
-  
   for (let i = 0; i < arrOfArr.length; i += 1) {
-    let calc;
-    calc = func(arrOfArr[i]);
-    if (func(arrOfArr[i]) > max) {
+    let calc = func(arrOfArr[i]);
+    if (calc > max) {
       max = calc;
     }
   }
-  
   return max;
 }
 
-// Задание 3
 function worker2(arr) {
   let min = arr[0];
   let max = arr[0];
- 
   for (let i = 0; i < arr.length; i += 1){
     if (arr[i] > max) {
       max = arr[i];
@@ -57,6 +44,5 @@ function worker2(arr) {
       min = arr[i];
     }
   }
-  
   return Math.abs(max - min);
 }
