7e890c22e0db1d22af49e4d37616fc5ac2198614
Piskun_Ivan <tw1st3r111@mail.ru>
2022-12-13 20:23:03 +0300

    task3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,32 +1,10 @@
 function compareArrays(arr1, arr2) {
-  let result = compareArrays.every((array) => sum === sum2);
-
-  function sumArray1() {
-    for (let element of arr1) {
-      let sum;
-      sum += arr1[element];
-      break;
-    }
-    return sum;
-  }
-  
-  function sumArray2() {
-    for (let element of arr2) {
-      let sum2;
-      sum2 += arr2[element];
-      break;
-    }
-    return sum2;
-  }
-  
+  let result = compareArrays.every((arr1, arr2) => arr1.length === arr2.length);
   return result; // boolean
 }
 compareArrays([8, 9], [6]);
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  resultArr = arr.filter(number => number > 0 ).filter(number => number % 3 == 0).map(number * 10)
-
+  let resultArr = arr.filter(number => number > 0 ).filter(number => number % 3 == 0).map(number * 10)
   return resultArr; // array
 }
