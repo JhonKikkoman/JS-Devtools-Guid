75a131dbc28d6fa06d7b47cef1ea5858aeb04a61
Piskun_Ivan <tw1st3r111@mail.ru>
2022-12-21 16:54:07 +0300

    task3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,10 +1,12 @@
 function compareArrays(arr1, arr2) {
-  let result = compareArrays.every((arr1, arr2) => arr1.length === arr2.length);
-  return result; // boolean
+  if (arr1.length === arr2.length) {
+    let result = compareArrays.every((element, index) => arr1[element] === arr2[element] && arr1[index] === arr2[index]);
+    return result; // boolean
+  }
 }
 compareArrays([8, 9], [6]);
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter(number => number > 0 ).filter(number => number % 3 == 0).map(number * 10)
+  let resultArr = arr.filter(number => number > 0 ).filter(number => number % 3 == 0).map(number => number * 10)
   return resultArr; // array
 }
