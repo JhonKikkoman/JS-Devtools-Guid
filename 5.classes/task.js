7d6974381cecf8911fae813a891a4a65c851c02a
Piskun_Ivan <tw1st3r111@mail.ru>
2023-01-24 16:31:44 +0300

    task5

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -10,20 +10,79 @@ class PrintEditionItem {
       return  this.state = (this.state * 1.5);
     }
 
-}
-
-new PrintEditionItem('Gorkiy', '2312', '123');
-let editItem = new PrintEditionItem();
-
-editItem = {
-    set state(newNum) {
-        if (newNum < 0) {
-            this._state = 0;
-        } else if (newNum > 100) {
-            this._state = 100;
-        } else return this._state = newNum;
-    },
-    get state() {
-        return this._state;
+    editItem = {
+        set state(newNum) {
+            if (newNum < 0) {
+                this._state = 0;
+            } else if (newNum > 100) {
+                this._state = 100;
+            } else return this._state = newNum;
+        },
+        get state() {
+            return this._state;
+        }
     }
 }
+
+let Gorkiy = new PrintEditionItem('Gorkiy', '2312', '123');
+
+class Magazine extends PrintEditionItem {
+    constructor() {
+        super(name, releaseDate, pagesCount)
+        this.type = "magazine";
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor(author) {
+        super(name, releaseDate, pagesCount)
+        this.author = author;
+        this.type = "book";
+    }
+    
+}
+
+class NovelBook extends Book {
+    constructor() {
+        super();
+        this.type = "novel";
+    }
+}
+
+class FantasticBook extends Book {
+    constructor() {
+        super();
+        this.type = "fantastic";
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor() {
+        super();
+        this.type = "detective";
+    }
+}
+
+class Library {
+    constructor(name, books) {
+        this.name = "";
+        this.books = [];
+    }
+    addBook(book) {
+        if(state > 30) {
+            this.books.push(book)
+        }
+    }
+    findBookBy(type, value) {
+        if(type === true && value === true) {
+            return this.name;
+        }
+    }
+    giveBookByName(bookName) {
+        if(bookName === true) {
+            this.books.splice(bookName);
+        } else return null;
+    }
+
+}
+
