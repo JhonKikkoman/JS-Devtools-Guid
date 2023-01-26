cfc344fa9d2cae6531b1db77c64b82a0c3859a09
Piskun_Ivan <tw1st3r111@mail.ru>
2023-01-26 19:06:28 +0300

    task6

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,15 +1,15 @@
 function parseCount(argX) {
-    if (parseInt(argX) === NaN) {
-        throw 'Невалидное значение'
+    if (isNaN(parseFloat(argX))) {
+        throw new Error ('Невалидное значение');
     }
-    return parseInt(argX)
+    return parseFloat(argX)
 } 
 
 function validateCount(argY) {
     try {
-       return parseCount(argY);  
+        return parseCount(argY);  
     } catch (error) {
-        console.error(error);
+        throw new Error(error);
     }
 
 }
@@ -18,8 +18,22 @@ console.log(parseCount('wads'));
 class Triangle {
     constructor (sideA, sideB, sideC) {
         if ((sideA + sideB) < sideC || (sideB + sideC) < sideA || (sideA + sideC) < sideB) {
-            throw "Треугольник с такими сторонами не существует"
-        }
-        
+            throw new Error ("Треугольник с такими сторонами не существует");
+        } 
     }
+    getPerimeter() {
+        let perimeter = sideA + sideB + sideC;
+    }
+    getArea() {
+        let area = (sideA + sideB + sideC) / 2;
+    }
+}
+
+function getTriangle(sideA, sideB, sideC) {
+    try {
+        return new Triangle;
+    } catch (error) {
+        throw new Triangle.getArea().getPerimeter()("Ошибка! Треугольник не существует");
+    }
+    
 }
