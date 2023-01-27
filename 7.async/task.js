024fe6210d90710d69ca3f0c1790b5ed1dd2b8e9
Piskun_Ivan <tw1st3r111@mail.ru>
2023-01-27 13:16:50 +0300

    task7

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,5 +1,15 @@
 class AlarmClock {
     constructor () {
-
+        this.alarmCollection = [];
+        this.timerId = '';
     }
+    addClock() {
+        (`HH:MM`) => {
+
+        }
+    }
+    removeClock() {
+        
+    }
+
 } 
