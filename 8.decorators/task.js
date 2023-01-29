06df2fcb6cd1a7221ecff6bc9018329c76deb07f
Piskun_Ivan <tw1st3r111@mail.ru>
2023-01-29 23:13:19 +0300

    task8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,5 +1,23 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  cache = [];
+  function wrapper(...args) {
+    const hash = args.join(",");
+    const objectInCache = cache.find((item) => hash[item] === cache[hash] );
+    if (!objectInCache) {
+      console.log("Из кэша: " + cache[hash]);
+      return "Из кэша: " + cache[hash];
+    }
+
+    let result = func(...args);
+    cache.push({hash});
+    if (cache.length > 5) {
+      cache.unshift(hash)
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
+
 }
 
 
