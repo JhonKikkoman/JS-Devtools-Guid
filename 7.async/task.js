f450a61e88289dcab643baa597a46a374e0f00e7
Piskun_Ivan <tw1st3r111@mail.ru>
2023-02-03 16:30:47 +0300

    task7

--- 7.async/task.js
+++ 7.async/task.js
@@ -2,26 +2,47 @@ class AlarmClock {
     constructor () {
         this.alarmCollection = [];
         this.timerId = '';
-        this.time = time;
-        this.callback = callback;
     }
     addClock(time, callback, id) {
-        this.time = `HH:MM`;
-        this.callback = () => {};
-        this.id = id;
+        time = `HH:MM`;
+        callback = () => {};
+        id = id;
 
-        if (this.id === undefined) {
+        if (id === undefined) {
             throw new Error('error text');
-        } else if (this.id === id) {
+        } else if (this.alarmCollection.some((elem) => this.alarmCollection[elem] === id)) {
             console.error("Ошибка");
             return;
         }
-        return this.alarmCollection.push({id: id, time: time, callback: callback});
+        this.alarmCollection = this.alarmCollection.push({id: id, time: time, callback: callback});
+        return arrLen === this.alarmCollection.length;
     }
     removeClock(id) {
-        this.id = id;
-        if (this.id === this.alarmCollection[id]) {
+        const arrLen = this.alarmCollection.length;
+        id = id;
+        if (id === this.alarmCollection[id]) {
             this.alarmCollection.filter(elem => elem.alarmCollection === true)
         }
+    }
+    getCurrentFormattedTime() {
+        return time;
+    }
+    start() {
+        function checkClock(addClock) {
+            if(addClock === getCurrentFormattedTime) {
+                return callback;
+            }
+            if (id === undefined){
+                setInterval(() => {
+                    for (let i = 0; i < this.alarmCollection.length; i++) {
+                        this.alarmCollection[i] = checkClock();
+                    }
+                    return 
+                }, 2000);
+            }
+        }
+    }
+    stop() {
+        
     }
 } 
