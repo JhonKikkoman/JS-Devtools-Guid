57a9b9a2911544a223421c85648c0e9498b9e364
Piskun_Ivan <tw1st3r111@mail.ru>
2023-02-06 17:27:49 +0300

    task7

--- 7.async/task.js
+++ 7.async/task.js
@@ -3,42 +3,34 @@ class AlarmClock {
         this.alarmCollection = [];
         this.timerId = '';
     }
-    addClock(time, callback, id) {
-        time = `HH:MM`;
-        callback = () => {};
-        id = id;
-
+    addClock() {
         if (id === undefined) {
             throw new Error('error text');
-        } else if (this.alarmCollection.some((elem) => this.alarmCollection[elem] === id)) {
+        } else if (this.alarmCollection.some((elem) => elem === id)) {
             console.error("Ошибка");
             return;
         }
-        this.alarmCollection = this.alarmCollection.push({id: id, time: time, callback: callback});
+        this.alarmCollection = this.alarmCollection.push({id, time, callback});  
+    }
+    removeClock() {
+        const arrLen = this.alarmCollection.length;
+        this.alarmCollection = this.alarmCollection.filter(elem => elem.alarmCollection === true);
         return arrLen === this.alarmCollection.length;
     }
-    removeClock(id) {
-        const arrLen = this.alarmCollection.length;
-        id = id;
-        if (id === this.alarmCollection[id]) {
-            this.alarmCollection.filter(elem => elem.alarmCollection === true)
-        }
-    }
     getCurrentFormattedTime() {
-        return time;
+        const time = Date();
+        return  String.time;
     }
     start() {
-        function checkClock(addClock) {
-            if(addClock === getCurrentFormattedTime) {
-                return callback;
-            }
-            if (id === undefined){
+        (addClock) => {
+            if (addClock.id != this.timerId) {
                 setInterval(() => {
-                    for (let i = 0; i < this.alarmCollection.length; i++) {
-                        this.alarmCollection[i] = checkClock();
-                    }
-                    return 
-                }, 2000);
+                    this.alarmCollection.forEach(() => { if(addClock.time === getCurrentFormattedTime) {
+                                                            return addClock.callback;
+                                                         } 
+                    })
+                    return start();
+                });
             }
         }
     }
